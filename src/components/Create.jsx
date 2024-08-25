f1fc5cce71a9330776745aa37ef812720bb8dfc6
ssushilgit <shresthasushil741@gmail.com>
2024-08-26 02:05:38 +0545

    Home Page and Create

---
+++ src/components/Create.jsx
@@ -0,0 +1,113 @@
+import axios from 'axios';
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+
+
+const Create = () => {
+    const [values, setValues] = useState({
+        profilePicture: '',
+        name: '',
+        username: '',
+        email: '',
+        phone: '',
+        dob: '',
+        city: '',
+        district: '',
+        province: '',
+        country: 'Nepal' // default to Nepal
+    })
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axios.post('http://localhost:3000/users', values)
+            .then(res => {
+                console.log(res);
+                navigate('/');
+
+            })
+            .catch(err => console.log(err));
+    }
+    const handleFileChange = (e) => {
+        setValues({ ...values, profilePicture: e.target.files[0] });
+    };
+
+    return (
+        <div className='d-flex mt-5 w-100 vh-100 justify-content-center align-items-center '>
+            <div className="border bg-white shadow px-5 pt-3 pb-5 rounded">
+                <h1>Add a User</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-2">
+                        <label htmlFor="name">Name:</label>
+                        <input type="text" name='name' className='form-control' placeholder='Enter Name'
+                            onChange={e => setValues({ ...values, name: e.target.value })} />
+                    </div>
+                    <div className="mb-2">
+                        <label htmlFor="username">Username:</label>
+                        <input type="text" name='username' className='form-control' placeholder='Enter Username'
+                            onChange={e => setValues({ ...values, username: e.target.value })} />
+                    </div>
+                    <div className="mb-2">
+                        <label htmlFor="email">Email:</label>
+                        <input type="email" name='email' className='form-control' placeholder='Enter Email'
+                            onChange={e => setValues({ ...values, email: e.target.value })} />
+                    </div>
+                    <div className="mb-2">
+                        <label htmlFor="phone">Phone:</label>
+                        <input type="text" name='phone' className='form-control' placeholder='Enter Phone'
+                            onChange={e => setValues({ ...values, phone: e.target.value })} />
+                    </div>
+                    <div className="mb-2">
+                        <label htmlFor="dob">DOB:</label>
+                        <input type="date" name='dob' className='form-control'
+                            onChange={e => setValues({ ...values, dob: e.target.value })} />
+                    </div>
+                    <div className="mb-2">
+                        <label htmlFor="city">City:</label>
+                        <input type="text" name='city' className='form-control' placeholder='Enter City'
+                            onChange={e => setValues({ ...values, city: e.target.value })} />
+                    </div>
+                    <div className="mb-2">
+                        <label htmlFor="district">District:</label>
+                        <input type="text" name='district' className='form-control' placeholder='Enter District'
+                            onChange={e => setValues({ ...values, district: e.target.value })} />
+                    </div>
+                    <div className="mb-2">
+                        <label htmlFor="province">Province:</label>
+                        <select name='province' className='form-control'
+                            onChange={e => setValues({ ...values, province: e.target.value })}>
+                            <option value="">Select Province</option>
+                            <option value="Province 1">Koshi Province</option>
+                            <option value="Province 2">Madhesh Province</option>
+                            <option value="Province 3">Bagmati Province</option>
+                            <option value="Province 4">Gandaki Province</option>
+                            <option value="Province 5">Lumbini Province</option>
+                            <option value="Province 6">Karnali Province</option>
+                            <option value="Province 7">Sudurpashchim Province</option>
+                        </select>
+                    </div>
+                    <div className="mb-2">
+                        <label htmlFor="country">Country:</label>
+                        {/* <select name='country' className='form-control' value={values.country} 
+                onChange={e => setValues({ ...values, country: e.target.value })}>
+                  {countries.map((country, index) => (
+                    <option key={index} value={country}>{country}</option>
+                  ))}
+                </select> */}
+                    </div>
+
+                    <div className="mb-2">
+                        <label htmlFor="profilePicture">Profile Picture:</label>
+                        <input type="file" name='profilePicture' className='form-control'
+                            onChange={handleFileChange} />
+                    </div>
+                    <button className='btn btn-success'>Submit</button>
+                    <Link to='/' className='btn btn-primary ms-3'>Back</Link>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default Create
---
+++ src/components/Home.jsx
@@ -0,0 +1,120 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    axios.get('http://localhost:3000/users')
+      .then(res => setData(res.data))
+      .catch(err => console.log(err));
+  }, []);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5;
+
+  const lastItem = currentPage * itemsPerPage;
+  const firstItem = lastItem - itemsPerPage;
+  const currentItems = data.slice(firstItem, lastItem);
+
+  const nextPage = () => {
+    setCurrentPage(prevPage => prevPage + 1);
+  };
+
+  const prevPage = () => {
+    setCurrentPage(prevPage => prevPage - 1);
+  };
+
+  const handleDelete = (id) =>{
+    const confirm = window.confirm("Would you like to delete?");
+    if(confirm){
+        axios.delete('http://localhost:3000/users/'+id)
+        .then(res =>{
+            location.reload()
+        })
+        .catch(err => console.log(err))
+    }
+}
+  
+
+  return (
+    <div className='d-flex flex-column justify-content-center align-items-center bg-light vh-100'>
+      <h1>List of Users</h1>
+      <div className="w-90 rounded bg-white border shadow p-4">
+        <div className="d-flex justify-content-end">
+          <Link to='/create' className='btn btn-success'>Add +</Link>
+        </div>
+        <table className='table table-striped'>
+          <thead>
+            <tr>
+              <th>Profile</th>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Username</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>DOB</th>
+              <th>City</th>
+              <th>District</th>
+              <th>Province</th>
+              <th>Country</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {currentItems.map((d, i) => (
+              <tr key={i}>
+                <td>
+                  {/* {d.profilePicture ? (
+                    <img
+                      src={`/path/to/profile-pictures/${d.profilePicture}`}
+                      alt={`${d.name}'s Profile`}
+                      width="50"
+                      height="50"
+                    />
+                  ) : (
+                    'No Image'
+                  )} */}
+                </td>
+                <td>{firstItem + i + 1}</td>
+                <td>{d.name}</td>
+                <td>{d.username}</td>
+                <td>{d.email}</td>
+                <td>{d.phone}</td>
+                <td>{d.dob}</td>
+                <td>{d.city}</td>
+                <td>{d.district}</td>
+                <td>{d.province}</td>
+                <td>{d.country}</td>
+                <td>
+                  <Link to={`read/${d.id}`} className='btn btn-sm btn-secondary mx-1 my-2'>Read</Link>
+                  <Link to={`update/${d.id}`} className='btn btn-sm btn-primary mx-2'>Edit</Link>
+                  <button onClick={e => handleDelete(d.id)} className='btn btn-sm btn-danger'>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className="d-flex justify-content-between">
+          <button
+            className='btn btn-primary'
+            onClick={prevPage}
+            disabled={currentPage === 1}
+          >
+            Previous
+          </button>
+          <button
+            className='btn btn-primary'
+            onClick={nextPage}
+            disabled={currentPage * itemsPerPage >= data.length}
+          >
+            Next
+          </button>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Home
